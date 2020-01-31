b71bd8d33589f1503252d71207bc5aa256037ad1
Seth Falco <seth@elypia.org>
2020-01-31 04:09:27 +0100

    Changed Version to 0.1.0 to Indicate it's not Stable

--- src/ImageCaster/BuildSteps/ExifBuildStep.cs
+++ src/ImageCaster/BuildSteps/ExifBuildStep.cs
@@ -30,7 +30,7 @@ namespace ImageCaster.BuildSteps
                 exifProfile = new ExifProfile();
             }
 
-            exifProfile.SetValue(ExifTag.Software, "ImageCaster 1.0.0");
+            exifProfile.SetValue(ExifTag.Software, "ImageCaster 0.1.0");
             magickImage.AddProfile(exifProfile);
             
             context.Next(magickImage);
