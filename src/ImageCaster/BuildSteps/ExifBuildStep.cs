09f04950706f0868c2e40e33c64e53cdba4697ee
Seth Falco <seth@elypia.org>
2020-02-02 06:38:35 +0100

    Incremented version to 0.1.10, Removed .dockerignore, cleaned up csproj

--- src/ImageCaster/BuildSteps/ExifBuildStep.cs
+++ src/ImageCaster/BuildSteps/ExifBuildStep.cs
@@ -30,7 +30,7 @@ namespace ImageCaster.BuildSteps
                 exifProfile = new ExifProfile();
             }
 
-            exifProfile.SetValue(ExifTag.Software, "ImageCaster 0.1.9");
+            exifProfile.SetValue(ExifTag.Software, "ImageCaster 0.1.10");
             magickImage.AddProfile(exifProfile);
             
             context.Next(magickImage);
