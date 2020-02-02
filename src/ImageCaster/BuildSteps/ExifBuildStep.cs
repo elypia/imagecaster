ab80071dd067f6912d4c7884a5b55d958737831e
Seth Falco <seth@elypia.org>
2020-02-02 03:24:13 +0100

    Incremented Version to 0.1.7, And Made 3 Dockerfiles Now Instead of 1

--- src/ImageCaster/BuildSteps/ExifBuildStep.cs
+++ src/ImageCaster/BuildSteps/ExifBuildStep.cs
@@ -30,7 +30,7 @@ namespace ImageCaster.BuildSteps
                 exifProfile = new ExifProfile();
             }
 
-            exifProfile.SetValue(ExifTag.Software, "ImageCaster 0.1.7");
+            exifProfile.SetValue(ExifTag.Software, "ImageCaster 0.1.8");
             magickImage.AddProfile(exifProfile);
             
             context.Next(magickImage);
