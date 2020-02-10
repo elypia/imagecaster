2368d194caea925f4a0b19e36e8bc932fc8101f6
Seth Falco <seth@elypia.org>
2020-02-10 19:44:52 +0100

    Split the project between a library/test project, plus CLI and API implementations.

---
+++ src/ImageCasterCli/Commands/BuildCommand.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using ImageCaster.Api;
+using ImageCaster.BuildSteps;
+using ImageCaster.Configuration;
+using ImageCaster.Extensions;
+using ImageMagick;
+using NLog;
+
+namespace ImageCaster.Commands
+{
+    /// <summary>
+    /// Using the <see cref="Export"/> configuration to export the input
+    /// in all desired ouput images.
+    /// </summary>
+    public class BuildCommand : ICliCommand
+    {
+        /// <summary>Logging with NLog.</summary>
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>The collector implementation in use, determines how files are found.</summary>
+        public ICollector Collector { get; }
+        
+        /// <summary>The user defined configuration to export images.</summary>
+        public ImageCasterConfig Config { get; }
+        
+        /// <param name="collector"><see cref="Collector"/></param>
+        /// <param name="config"><see cref="Config"/></param>
+        public BuildCommand(ICollector collector, ImageCasterConfig config)
+        {
+            this.Collector = collector.RequireNonNull();
+            this.Config = config.RequireNonNull();
+        }
+
+        public Command Configure()
+        {
+            return new Command("build", "Export the output images from the source")
+            {
+                Handler = CommandHandler.Create(Execute)
+            };
+        }
+        
+        /// <summary>Start building all images.</summary>
+        /// <exception cref="InvalidOperationException">If the configuration is malformed.</exception>
+        public int Execute()
+        {
+            Logger.Trace("Executed build command, started working.");
+
+            Export export = Config.Export;
+
+            if (export == null)
+            {
+                Logger.Warn("Build command was called, but no export configuration was defined, doing nothing.");
+                return (int)ExitCode.Normal;
+            }
+            
+            string input = export.Input;
+
+            if (input == null)
+            {
+                Logger.Fatal("Build command was called but export.input configuration was not specified, this is required.");
+                return (int)ExitCode.MissingConfigField;
+            }
+            
+            List<ResolvedFile> resolvedFiles = Collector.Collect(input);
+            Logger.Debug("Found {0} files matching collection pattern.", resolvedFiles.Count);
+
+            List<IBuildStep> pipeline = new List<IBuildStep>()
+            {
+                new ExifBuildStep(),
+                new ModulateBuildStep(),
+                new ResizeBuildStep(),
+                new WriteBuildStep()
+            };
+            
+            foreach (IBuildStep step in pipeline)
+            {
+                step.Configure(Collector, Config);
+            }
+
+            foreach (ResolvedFile resolvedFile in resolvedFiles)
+            {
+                FileInfo fileInfo = resolvedFile.FileInfo;
+                PipelineContext context = new PipelineContext(pipeline, resolvedFile);
+                context.AppendPath("export");
+            
+                using (MagickImage magickImage = new MagickImage(fileInfo))
+                {
+                    context.Next(magickImage);
+                }
+
+                Logger.Info("Finished all exports for {0}.", resolvedFile);
+            }
+
+            return (int)ExitCode.Normal;
+        }
+    }
+}
